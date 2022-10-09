3299be97e8a814015c644b5b291bbf83654700fd
Abhisek Maiti <abhisekmaiti19@gmail.com>
2022-10-09 06:28:28 +0530

    done2

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,13 +6,20 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private Boolean isrun1 = false;
+    private Boolean isrun2 =  false;
+    private Boolean isrun3 = false;
+
+
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
-        if(isVeg)
+        if(isVeg )
         {
             this.price = 300;
             this.bill = "Base Price Of The Pizza: 300\n";
+
         }else
         {
             this.price = 400;
@@ -26,27 +33,39 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
-        this.price = this.price +80;
-        this.bill += "Extra Cheese Added: 80\n";
+
+        if(isrun1 == false)
+        {
+            this.price = this.price +80;
+            this.bill += "Extra Cheese Added: 80\n";
+            isrun1 = true;
+
+        }
+
     }
 
     public void addExtraToppings(){
         // your code goes here
-        if(isVeg)
-        {
-            this.price = this.price+70;
-            this.bill += "Extra Toppings Added: 70\n";
-        }else
-        {
-            this.price = this.price + 120;
-            this.bill += "Extra Cheese Added: 120\n";
+        if(isrun2 == false) {
+            if (isVeg) {
+                this.price = this.price + 70;
+                this.bill += "Extra Toppings Added: 70\n";
+                isrun2 = true;
+            } else {
+                this.price = this.price + 120;
+                this.bill += "Extra Cheese Added: 120\n";
+                isrun2 = true;
+            }
         }
     }
 
     public void addTakeaway(){
         // your code goes here
-        this.price = this.price + 20;
-        this.bill += "Paperbag Added: 20\n";
+        if(isrun3 == false) {
+            this.price = this.price + 20;
+            this.bill += "Paperbag Added: 20\n";
+            isrun3 = true;
+        }
     }
 
     public String getBill(){
